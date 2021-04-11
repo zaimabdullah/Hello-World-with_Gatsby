d93cbd4694febb734298a39c4ab5bcbe7b9dc367
Your Name <you@example.com>
2021-04-11 16:44:35 +0800

    add and edit  index, header, about, contact page

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,5 +1,14 @@
 import React from "react"
+import { Link } from "gatsby"
+import Header from "../components/header"
 
 export default function Home() {
-  return <div>Hello world!</div>
+  return (
+  <div style={{ color: `purple` }}>
+    <Link to="/contact/">Contact</Link>
+    <Header headerText="Hello Gatsby!" />
+    <p>What a world.</p>
+    <img src="https://images.unsplash.com/photo-1617103099853-6dd647eb1489?ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=400&q=200" alt="" />
+  </div>
+  );
 }
